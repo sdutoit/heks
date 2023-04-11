1d8bcffb9721635c7166d41bd4fd77734bcb01de
Stefanus Du Toit <sjdutoit@gmail.com>
2023-04-11 13:50:13 +0200

    Refactor and handle signals

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,39 @@ use crossterm::{
     event::{poll, read, KeyCode},
     terminal::{disable_raw_mode, enable_raw_mode},
 };
-use std::{io, time::Duration};
+use futures::{stream::FuturesUnordered, StreamExt};
+use std::{io, process::exit, time::Duration};
+use tokio::signal::unix::{signal, SignalKind};
 use tokio::time::{sleep_until, Instant};
 
-async fn event_loop() -> io::Result<()> {
-    let mut next_tick = Instant::now();
-    let mut done = false;
+struct EventLoop {
+    done: bool,
+}
 
-    while !done {
-        sleep_until(next_tick).await;
+impl EventLoop {
+    pub fn new() -> EventLoop {
+        EventLoop { done: false }
+    }
 
+    pub async fn run(&mut self) -> io::Result<()> {
+        let mut next_tick = Instant::now();
+
+        while !self.done {
+            sleep_until(next_tick).await;
+
+            self.tick()?;
+
+            let now = Instant::now();
+            next_tick += Duration::from_micros(16667);
+            if next_tick < now {
+                next_tick = now;
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn tick(&mut self) -> io::Result<()> {
         while poll(Duration::from_secs(0))? {
             let event = read()?;
             match event {
@@ -19,7 +42,7 @@ async fn event_loop() -> io::Result<()> {
                 crossterm::event::Event::FocusLost => {}
                 crossterm::event::Event::Key(key) => {
                     if key.code == KeyCode::Esc {
-                        done = true;
+                        self.done = true;
                     }
                 }
                 crossterm::event::Event::Mouse(_) => {}
@@ -28,20 +51,38 @@ async fn event_loop() -> io::Result<()> {
             }
         }
 
-        let now = Instant::now();
-        next_tick += Duration::from_micros(16667);
-        if next_tick < now {
-            next_tick = now;
-        }
+        Ok(())
     }
+}
 
-    Ok(())
+fn setup_signal_handler() {
+    tokio::spawn(async move {
+        let mut handlers: Vec<_> = [
+            SignalKind::hangup(),
+            SignalKind::interrupt(),
+            SignalKind::terminate(),
+        ]
+        .iter()
+        .map(|&kind| signal(kind).unwrap())
+        .collect();
+
+        let mut signals: FuturesUnordered<_> =
+            handlers.iter_mut().map(|handler| handler.recv()).collect();
+
+        signals.next().await;
+
+        let _ = disable_raw_mode();
+        exit(1);
+    });
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> io::Result<()> {
     enable_raw_mode()?;
-    event_loop().await?;
+    setup_signal_handler();
+
+    let mut event_loop = EventLoop::new();
+    event_loop.run().await?;
     disable_raw_mode()?;
 
     Ok(())
