1eaef9fd542b8799b8313ab19f4d6a0bf062bca4
Stefanus Du Toit <sjdutoit@gmail.com>
2023-05-01 10:01:40 +0200

    Super duplicated, but unicode display shows cursor!

--- src/lib.rs
+++ src/lib.rs
@@ -114,6 +114,7 @@ struct UnicodeDisplay {
     style: Style,
     data: Vec<u8>,
     data_start: u64,
+    pub cursor: Cursor,
 }
 
 impl UnicodeDisplay {
@@ -122,6 +123,7 @@ impl UnicodeDisplay {
             style: Style::default(),
             data: vec![],
             data_start: 0,
+            cursor: Cursor::new(0, 0),
         }
     }
 
@@ -186,23 +188,41 @@ fn render_unicode_byte(byte: u8) -> String {
     }
 }
 
-fn render_unicode(bytes: &[u8]) -> String {
+fn render_unicode(bytes: &[u8], bytes_start: u64, cursor: Cursor) -> Vec<Spans> {
     let mut column = 0;
-    let mut result = String::new();
+    let mut lines: Vec<Spans> = vec![];
+    let mut spans = vec![];
+
+    let mut byte = bytes_start;
+
+    let cursor_style = Style::default().bg(color(0, 96, 0)).fg(color(96, 255, 96));
     bytes.iter().map(|b| render_unicode_byte(*b)).for_each(|s| {
-        result.push_str(s.as_str());
+        let style = if cursor.contains(byte) {
+            cursor_style
+        } else {
+            Style::default()
+        };
+        spans.push(Span::styled(s, style));
         column += 1;
         if column == COLUMNS {
-            result.push('\n');
+            lines.push(Spans::from(spans.clone()));
+            spans.clear();
             column = 0;
         }
+
+        byte += 1;
     });
-    result
+
+    if !spans.is_empty() {
+        lines.push(Spans::from(spans.clone()));
+    }
+
+    lines
 }
 
 impl Widget for UnicodeDisplay {
     fn render(self, area: tui::layout::Rect, buf: &mut tui::buffer::Buffer) {
-        let text = render_unicode(&self.data);
+        let text = render_unicode(&self.data, self.data_start, self.cursor);
 
         Paragraph::new(text).style(self.style).render(area, buf);
     }
@@ -304,6 +324,7 @@ impl App {
         self.hex_display
             .set_data(slice.data.to_vec(), slice.location.start);
 
+        self.unicode_display.cursor = self.cursor;
         self.unicode_display
             .set_data(slice.data.to_vec(), slice.location.start);
 
